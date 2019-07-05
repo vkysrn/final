3565cf995e8026338acf19fcc361fe2984157689
vkysrn <52466034+vkysrn@users.noreply.github.com>
2019-07-05 13:57:46 +0530

    make it better

--- wp-config.php
+++ wp-config.php
@@ -17,19 +17,19 @@
  *
  * @package WordPress
  */
-
+mysql://bf38ff8efc3f43:da01676a@us-cdbr-iron-east-02.cleardb.net/heroku_10a4c72a306c488?reconnect=true
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'newzigma');
+define('DB_NAME', 'heroku_10a4c72a306c488');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'bf38ff8efc3f43');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'da01676a');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
